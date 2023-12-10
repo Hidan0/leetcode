0621ba8e91f2211cfc4ed0f8cf55bcdd9d26870a
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-12-10 23:09:47 +0100

    2264

---
+++ 2264_Largest3-Same-DigitNumberInString.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <list>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  string largestGoodInteger(string num) {
+    char prev = ' ';
+    int prevc = 0;
+    list<string> nums;
+
+    for (int i = 0; i < num.length() - 2; i++) {
+      string n = num.substr(i, 3);
+      if (n[0] == n[1] && n[1] == n[2]) {
+        nums.push_back(n);
+      }
+    }
+
+    string max = "-1";
+
+    for (const auto n : nums) {
+      if (atoi(n.c_str()) > atoi(max.c_str())) {
+        max = n;
+      }
+    }
+
+    return max == "-1" ? "" : max;
+  }
+};
