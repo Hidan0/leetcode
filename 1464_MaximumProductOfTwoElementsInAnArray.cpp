0f3aefe28148a315e060493b66ff21326f34090d
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-12-12 01:11:28 +0100

    1464

---
+++ 1464_MaximumProductOfTwoElementsInAnArray.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProduct(vector<int> &nums) {
+
+    sort(nums.begin(), nums.end());
+    return (nums[nums.size() - 1] - 1) * (nums[nums.size() - 2] - 1);
+  }
+};
